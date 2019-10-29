ead27ca926313f4e3e3a56a8ab3fa79bf0601d71
astutz96 <astutz96.development@protonmail.com>
2019-10-28 22:50:29 -0400

    Implement function to evaluate cellstate with tests

--- GameOfLife.Library.Tests/UnitTest1.cs
+++ GameOfLife.Library.Tests/UnitTest1.cs
@@ -1,20 +1,77 @@
 using NUnit.Framework;
+using GameOfLife.Library;
 
 namespace Tests
 {
     public class Tests
     {
         /* Game Of Life Rules
-         * Any live cell with fewer than two live neighbours dies.
-         * Any live cell with two or three live neighbours lives.
-         * Any live cell with more than three live neighbours dies.
-         * Any dead cell with exactly three live neighbours become a live cell
+         * Any live cell with fewer than two live neighbors dies.
+         * Any live cell with two or three live neighbors lives.
+         * Any live cell with more than three live neighbors dies.
+         * Any dead cell with exactly three live neighbors become a live cell
          */
 
         [Test]
-        public void Test1()
+        public void LiveCell_FewerThanTwoLiveneighbors_Dies([Values(0,1)] int liveNeighbors)
         {
-            Assert.Pass();
+            var currentState = CellState.Alive;
+
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+
+            Assert.AreEqual(CellState.Dead, newState);
+        }
+
+        [Test]
+        public void LiveCell_TwoOrThreeLiveNeighbors_Lives([Values(2,3)] int liveNeighbors)
+        {
+            var currentState = CellState.Alive;
+
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+
+            Assert.AreEqual(CellState.Alive, newState);
+        }
+
+        [Test]
+        public void LiveCell_MoreThanThreeLiveNeighbors_Dies([Range(4,8)] int liveNeighbors)
+        {
+            var currentState = CellState.Alive;
+
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+
+            Assert.AreEqual(CellState.Dead, newState);
+        }
+        [Test]
+        public void DeadCell_ExactlyThreeLiveNeighbors_Lives([Values(3)] int liveNeighbors)
+        {
+            var currentState = CellState.Dead;
+
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+
+            Assert.AreEqual(CellState.Alive, newState);
+
+        }
+
+        [Test]
+        public void DeadCell_FewerThanThreeLiveNeighbors_StaysDead([Range(0,2)] int liveNeighbors)
+        {
+            var currentState = CellState.Dead;
+
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+
+            Assert.AreEqual(CellState.Dead, newState);
+
+        }
+
+        [Test]
+        public void DeadCell_GreaterThanThreeLiveNeighbors_StaysDead([Range(4, 8)] int liveNeighbors)
+        {
+            var currentState = CellState.Dead;
+
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+
+            Assert.AreEqual(CellState.Dead, newState);
+
         }
     }
 }
